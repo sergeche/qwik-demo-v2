4119f98ea163d5068a3049da87b5c9a83caf0f79
Sergey Chikuyonok <serge.che@gmail.com>
2025-08-21 22:50:55 +0300

    Reactivity lost!

--- src/routes/[...all]/index.tsx
+++ src/routes/[...all]/index.tsx
@@ -7,14 +7,24 @@ export const usePageData = routeLoader$(async event => {
   if (!isServer) throw new Error('Server only function');
   console.log('get page data for locale', event.locale());
   const data = await BcCmsSidebarFetchData(event);
-  return { page: data };
+  return {
+    blocks: [
+      { id: 'sidebar', data }
+    ]
+  };
 });
 
 export default component$(() => {
   const pageLoaded = usePageData();
   return (
     <>
-      <BcCmsSidebar data={pageLoaded.value.page} />
+      {pageLoaded.value.blocks.map(block => {
+        if (block.id === 'sidebar') {
+          // Lost reactivity!
+          return <BcCmsSidebar key={block.id} data={block.data} />;
+        }
+        return null;
+      })}
     </>
   );
 });
